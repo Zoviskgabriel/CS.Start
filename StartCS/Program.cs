4ca22855249e8dc3d8b3bff7de8f7c2fe30794ad
Gabriel Trimboli <104457830+Zoviskgabriel@users.noreply.github.com>
2025-08-18 11:31:07 -0300

    Atividade de Condicionais e operadores.

--- StartCS/Program.cs
+++ StartCS/Program.cs
@@ -6,7 +6,29 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1, n2;
+            int r;
+            Console.WriteLine("Qual a idade do primeiro aluno?");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a idade do segundo aluno?");
+            n2 = int.Parse(Console.ReadLine());
+
+            r = n1 + n2;
+
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("O primeiro aluno é mais velho que o segundo aluno");
+            }
+            if (n1 < n2)
+            {
+                Console.WriteLine("O segundo aluno é mais velho que o primeiro aluno");
+            }
+            if (n1 == n2)
+            {
+                Console.WriteLine("Os dois alunos têm a mesma idade");
+            }
         }
     }
 }
